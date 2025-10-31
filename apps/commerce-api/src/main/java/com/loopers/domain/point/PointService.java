f1ab580c73f02e0c2bfdc164e11b4c0991a6f38c
minor7295 <minor7295@gmail.com>
2025-10-31 11:04:33 +0900

    feat: 포인트 충전 서비스 로직 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
@@ -1,8 +1,11 @@
 package com.loopers.domain.point;
 
 import com.loopers.domain.user.User;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
@@ -16,6 +19,16 @@ public class PointService {
 
     public Point findByUserId(String userId) {
         return pointRepository.findByUserId(userId);
+    }
+
+    @Transactional
+    public Point charge(String userId, Long amount) {
+        Point point = pointRepository.findByUserId(userId);
+        if (point == null) {
+            throw new CoreException(ErrorType.NOT_FOUND, "포인트를 찾을 수 없습니다.");
+        }
+        point.charge(amount);
+        return pointRepository.save(point);
     }
 }
 
