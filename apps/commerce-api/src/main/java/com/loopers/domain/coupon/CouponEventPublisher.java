6ccf0990a6e25afa235aba82d4b3a027a33046d3
minor7295 <minor7295@gmail.com>
2025-12-11 00:44:41 +0900

    feat: 쿠폰 이벤트 처리하는 로직 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/coupon/CouponEventPublisher.java
@@ -0,0 +1,22 @@
+package com.loopers.domain.coupon;
+
+/**
+ * 쿠폰 도메인 이벤트 발행 인터페이스.
+ * <p>
+ * DIP를 준수하여 도메인 레이어에서 이벤트 발행 인터페이스를 정의합니다.
+ * 구현은 인프라 레이어에서 제공됩니다.
+ * </p>
+ *
+ * @author Loopers
+ * @version 1.0
+ */
+public interface CouponEventPublisher {
+
+    /**
+     * 쿠폰 적용 이벤트를 발행합니다.
+     *
+     * @param event 쿠폰 적용 이벤트
+     */
+    void publish(CouponEvent.CouponApplied event);
+}
+
